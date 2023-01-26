b37a109501986b954d2d4ee99d1e78471e6e73ae
Nibesh Adhikari <nibeshadhikari@ideabreed-rawan.local>
2023-01-26 16:09:08 +0545

    update readme with algorithm

---
+++ backpropagation.cpp
@@ -0,0 +1,12 @@
+/*Assign small random variable to weight (-0.5 to +0.5)
+Use feed forward method to calculate o/p(yk) by supply inputs
+calculate delta for o/p layer (dk=(tk-yk)f(yink)
+calculate delta for hidden layer dj = dinj f'(zinj)  |  dinj=sum(dk,wjk)
+calculate dw, dwij=axi,di   |  dwjk = azj,dk
+adjust neuron weight  wif(new)=wij(old)  + dwij  |  wjk(new)=wjk(old) + dwjk
+Repeate from step2 until supplied cycyle threshold level */
+
+#include<stdio.h>
+#include<math.h>
+
+    
